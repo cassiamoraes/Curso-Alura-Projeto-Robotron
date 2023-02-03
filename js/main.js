d0458524651a44d198344acd391f80cc8114aafe
Cassia Moraes <ccassiamoraess@gmail.com>
2023-02-03 17:05:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,6 +35,36 @@ const pecas = {
     }
 }
 
+//função chama o nome do botão informado na função onclick no html e troca o nome conforme nome da imagem salva na pasta
+//1ª solução
+/*function trocaImagem(cor){
+    document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";  
+}*/
+
+//2ªsolução
+/*function trocaImagem(elemento){
+    const robo = document.querySelector(".robo")
+    const img = "img/Robotron 2000 - "
+    const png = ".png"
+
+    robo.src = img + elemento + png
+    console.log(elemento)
+}*/
+
+//3ªsolução
+const btn = document.querySelectorAll("[data-botao]")
+
+btn.forEach((elemento) =>{
+    elemento.addEventListener("click", (evento) =>{
+       mudaCor(elemento)
+    })
+})
+
+function mudaCor(elemento){
+    const altera = elemento.firstChild
+    document.querySelector(".robo").src="img/Robotron 2000 - " + altera.textContent + ".png";
+}
+
 //obs.: o elemento é um parametro do html, pode ser um input, botão ou campo que recebe algum comando
 
 //comando que recebe a função com os dois parâmetros de conversão necessários, elemento = aos botões de + e -
